662fbbbc78a7e68debd59145b036dc6f766b88e9
Aloving <Ilegend2011@gmail.com>
2017-06-05 17:12:58 +0300

    #9 - interaction of components

--- app/todo-list/todo-list.component.ts
+++ app/todo-list/todo-list.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Todo } from '../shared/todo';
-import { todos } from '../shared/data';
 
 @Component({
 	moduleId: module.id,
@@ -10,7 +9,7 @@ import { todos } from '../shared/data';
 	styleUrls: ['todo-list.component.css']
 })
 export class TodoListComponent{
-	todos: Todo[] = todos;
+	@Input() todos: Todo[];
 
 	toggle(todo: Todo){
 		todo.completed = !todo.completed;
@@ -20,7 +19,7 @@ export class TodoListComponent{
 		let index = this.todos.indexOf(todo);
 
 		if(index > -1){
-			todos.splice(index, 1);
+			this.todos.splice(index, 1);
 		}else{
 			return;
 		}
